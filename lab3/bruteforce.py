e9f33edc6c217ff1d67920b6c07a926e9a6b1a3b
Salakapoi <reviewgamerkla@gmail.com>
2021-09-02 19:45:03 +0700

    It won't clear array!

--- lab3/bruteforce.py
+++ lab3/bruteforce.py
@@ -1,23 +1,56 @@
 class bruteForce:
 
     # Reading input file
+    """
     file = open("/input/Example/3.1.1.txt", "r")
     string = file.readline()
-    k = file.readline()
+    integer = file.readline()
     file.close()
+    """
+
+    #Simulate input
+    string = "GGPPGGGGPPPG"
+    integer = "3"
 
     # Processing the input
+    global arr
     arr = []
     for alphabet in string:
         arr.append(alphabet)
-    k = int(k)
-    global max
+    global k
+    k = int(integer)
+    global max_pickup
+    max_pickup = 0
+    global used_index
+    used_index = []
+    global cnt
+    cnt = 0
+
+    # Pick up passenger in unit range
+    def pick_up(index, unit):
+        cnt_pick = 0
+        p_index_left = index - unit
+        p_index_right = index + unit
+        if p_index_left >= 0 and arr[p_index_left] == "P" and not used_index.__contains__(p_index_left):
+            used_index.append(p_index_left)
+            cnt_pick += 1
+        elif p_index_right < len(arr) and arr[p_index_right] == "P" and not used_index.__contains__(p_index_right):
+            used_index.append(p_index_right)
+            cnt_pick += 1
+        return cnt_pick
 
     # Pairing Grab and Passenger
     for i in range(k):
-        cnt = 0
         for j in range(len(arr)):
-            
+            if arr[j] == "G":
+                cnt += pick_up(j,i)
+        used_index.clear # WHY. DID. YOUUU. NOT. CLEAR. ALL. OF. YOUR. ITEMSSSSSSSSSS!!!!!!!!!
+        if cnt > max_pickup:
+            max_pickup = int(cnt)
+            cnt = 0
+             
+    print("Output : " + str(max_pickup) + ".")
+
 """
  ---- Idea ----
     create tempArrayInput
