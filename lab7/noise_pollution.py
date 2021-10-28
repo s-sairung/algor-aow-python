f4e73fd8c38f8a8b3a1011e8081c09bd0d292012
s-sairung <siwakorn_pipe@hotmail.com>
2021-10-28 20:50:55 +0700

    Noise pollution all done

--- lab7/noise_pollution.py
+++ lab7/noise_pollution.py
@@ -1,3 +1,4 @@
+#
 #                           |
 #                        _oo0oo_
 #                       o8888888o
@@ -18,30 +19,95 @@
 #      =====`-.____`.___ \_____/___.-`___.-'=====
 #                        `=---='
 #
-#                  สาธุ วันพฤหัสเสร็จแน่ 99
+#                     สาธุ วันนี้แน่ 99
 #
+
+#           _.-/`)
+#          // / / )
+#       .=// / / / )
+#      //`/ / / / /
+#     // /     ` /
+#    ||         /
+#     \\       /
+#      ))    .'
+#     //    /
+#          /
+
+# ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+# ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+# ▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒█▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+# ▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒██▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+# ▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒██▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+# ▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒██▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+# ▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒██▒▒▒█▒▒▒▒▄███████▒▒
+# ▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒██▒▒█▒▒▒▒█▒▒▒▒██▀▒▒
+# ▒▒▄▒▒▒▒▒▒▒▒█▒▒█▄▒▒▒▒▒██▒█▒▒▒▒▒▒▒▄██▒▒▒▒
+# ▒▒█▄▒▒▒▒▒▒▄█▒▒██▄▒▒▒▒███████████▀▀▒▒▒▒▒
+# ▒▒▒████████▒▒▒▒▀████████▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+# ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+# ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+#      .-.
+#    __| |__
+#   [__   __]
+#      | |
+#      | |
+#      | |
+#      '-'
+
+#                           ____,......__
+#                      _, -'' _________,..i''--.._
+#                  ,.-'' ,.-''             '' ... '`-._
+#               ,/' _.-''                        '`-.  '-
+#             ,' ,-'                                 ':. `-
+#           ,' ,'                                       `. `.
+#          / ,'                                           `. \
+#        ,' /                       .|                      \ `.
+#       /  /                        '\                       \  \
+#      / ,'                       ,''`__                      `. \
+#     / /                         ':::.|                        \ \
+#    / ;                          |+-'i|                         : \
+#   ;  ;                         ,',''.|                         :  :
+#   ; /                         /,|- --|\                         \ :
+# +/ /                         ,;'| ;; |\\                         \ \
+# ; ;                         ,/__,\--i .L`                         : :
+# ;-;   /|                  ,''|||'    '+-:...                      : :
+#  ;._ |..\              _-'    |`      | _''-:`-.           ,''--'' : :
+#  |\ / \              ,-       |.\-   ,|/-   |   \         ,' /-    | |
+#  ; '`\ ;-         _-'     ,,. '`:`'ii,''   ,_    -.     _:..;:     : :
+# ;    -' ;+      ,+||,. _,' ,\...--'    ,,-i -`..   '-._,''''        : :
+# |     -:  '.. +'||| ||/  /' __,.....'-'  ,'   '-'-.    `  /         | |
+# |      \     .  |.  /,/;;,'' |          /         ''-..__'          | |
+# |       \   |    `-/' |/|     \        .              \             | |
+# |         ''|`._.-'   |'     __-`-.._....:                          | |
+# |            '             ,'  ''.---iii  '-_                       | |
+# :                        ,'      `\::\      `:'-.                  _; ;
+#  :                     ,'     ,._ |\::\      ':_.:=.._______..,--''; ;
+#  |                   /'    ,i:,'':+ \::\      | `_ \-._ .__     _. | |
+# -:-                /'   _,'' /      :`\:\     .   \ `\ '--.i---'   ; ;
+# : :             -;:. ,,-    /        ',i.:___/     \  '-.   '-..  ; ;
+# : :       ,-----  i;/'     '         /     '        `    '`-.._,:`;,;
+#  \ \       '''--.,-'     ,'        /'                ' .._____,.-+'/
+#   : \                   /       ,/'                             / ;
+#   :  :                 ||    _,'                               ;  ;
+#    \ :                  |    |                                 ; /
+#     \ \                  \    \                               / /
+#      \ `.                 `    \                            ,' /
+#       \  \                 `.  '.                          /  /
+#        `. \                  \__i.                        / ,'
+#          \ `.                 +''`.                     ,' /
+#           `. `.       ,+.._;:-i;.--:  .-.             ,' ,'
+#             `. `.    (\i.).-''-=-----+--`\          ,' ,'
+#               `. `-.  `+'.---.....|-.|__ +'''':-.,-' ,'
+#                 `-. `-.../       - ...:=-.|   ,-' ,-'
+#                    `-. `--._  :dg:       _.--' ,-'
+#                       `---._''---------''_.---'
+#                             ''---------''
+
 
 import os
 import copy
 import math as m
-
-class Vertice:
-
-    def __init__(self, id: int):
-        self.id = id
-
-
-class Matrix:
-    
-    def __init__(self, k: int, n: int):
-        self.k = k
-        self.matrix = [[m.inf] * n for i in range(n)]
-
-    def rows(self):
-        return len(self.matrix)
-
-    def copy_matrix(self):
-        return copy.deepcopy(self.matrix)
 
 
 def print_array(arr: list):
@@ -49,6 +115,7 @@ def print_array(arr: list):
         print(a)
 
 
+# ห้ามแก้เดี๋ยวเจ๊ง
 def floyd_warshall(w: list):
     n = len(w)
 
@@ -81,6 +148,38 @@ def floyd_warshall(w: list):
     return (arr_d[n], arr_pi[n])
 
 
+def floyd_warshall_noise_pollution_version(w: list):
+    n = len(w)
+
+    # initialize pi0
+    pi0 = [[0] * n for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i == j or w[i][j] == m.inf:
+                pi0[i][j] = None
+            else:
+                pi0[i][j] = i
+
+    d0 = copy.deepcopy(w)
+    arr_d = []
+    arr_d.append(d0)
+    arr_pi = []
+    arr_pi.append(pi0)
+    for k in range(1, n + 1):
+        d = [[m.inf] * n for i in range(n)]
+        pik = [[None] * n for i in range(n)]
+        for i in range(n): 
+            for j in range(n):
+                d[i][j] = min(arr_d[k - 1][i][j], max(arr_d[k - 1][i][k - 1], arr_d[k - 1][k - 1][j]))
+                if arr_d[k - 1][i][j] <= max(arr_d[k - 1][i][k - 1], arr_d[k - 1][k - 1][j]):
+                    pik[i][j] = arr_pi[k - 1][i][j]
+                else:
+                    pik[i][j] = arr_pi[k - 1][k - 1][j]
+        arr_d.append(d)
+        arr_pi.append(pik)
+    return (arr_d[n], arr_pi[n])
+
+
 def print_all_pair_shortest_path(pi: list, i: int, j: int):
     if i == j:
         print(i + 1)
@@ -90,9 +189,28 @@ def print_all_pair_shortest_path(pi: list, i: int, j: int):
         print_all_pair_shortest_path(pi, i, pi[i][j])
         print(j + 1)
 
-"""----------------------------อ่านไฟล์อยู่นี่ค่ะ-------------------------------------"""
+
+
+def print_all_pair_shortest_path_noise_pollution_version(d: list, pi: list, i: int, j: int):
+    if mee_path_maii(pi, i, j):
+        print("This path from " + str(i + 1) + " to " + str(j + 1) + " will produce a minimum decibel of " + str(d[i][j]))
+    else:
+        print("no path jaaa")
+    # else:
+    #     print_all_pair_shortest_path_noise_pollution_version(d, pi, i, pi[i][j])
+
+
+def mee_path_maii(pi: list, i: int, j: int):
+    if i == j:
+        return True
+    elif pi[i][j] == None:
+        return False
+    return mee_path_maii(pi, i, pi[i][j])
+
+
+"""----------------------------อ่านไฟล์อยู่นี่โว้ย-------------------------------------"""
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'input/ex.txt')
+filename = os.path.join(dirname, 'input/7.1.txt')
 f = open(filename)
 lines = [line.strip() for line in f.readlines()]
 f.close()
@@ -112,17 +230,20 @@ for line in range(1, edges + 1):
     j = int(j)
     w = int(w)
     weight_matrix[i - 1][j - 1] = w
-    # weight_matrix[j - 1][i - 1] = w # undirected graph แต่ต้องทำแบบนี้ทุกจุดใน code
+    weight_matrix[j - 1][i - 1] = w # undirected graph
     
 print(weight_matrix)
-tup = floyd_warshall(weight_matrix)
-print_array(tup[0])
-print_array(tup[1])
+tup = floyd_warshall_noise_pollution_version(weight_matrix)
+d = tup[0]
+pi = tup[1]
+print_array(d)
+print_array(pi)
 
 for line in range(edges + 1, edges + ans + 1):
     i, j = lines[line].split(" ")
     print("Path for i = " + i + ", j = " + j)
     i = int(i) - 1
     j = int(j) - 1
-    print_all_pair_shortest_path(tup[1], i, j)
+    # print_all_pair_shortest_path(pi, i, j)
+    print_all_pair_shortest_path_noise_pollution_version(d, pi, i, j)
     print("----------")
