bfc0ee7d61c51fee1461a814af97cbd6c0b784fd
s-sairung <siwakorn_pipe@hotmail.com>
2021-11-05 13:12:39 +0700

    extra test case left

--- lab8/exchange.py
+++ lab8/exchange.py
@@ -2,7 +2,7 @@ import os
 import math as m
 
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'input/test.txt')
+filename = os.path.join(dirname, 'input/8.6 extra.txt')
 f = open(filename)
 lines = [line.strip() for line in f.readlines()]
 f.close()
@@ -13,8 +13,8 @@ global values
 days = int(lines[0])
 values = [float(v) for v in lines[1].split(" ")]
 print("----------------------------input--------------------------------------------------------")
-print(days)
-print(values)
+# print(days)
+# print(values)
 
 
 # O(n^2)
@@ -53,45 +53,110 @@ def keng_kumrai_Brute_Force():
 
 def keng_kumrai_Divide_And_Conquer(start: int, end: int): # start = 0, end = 4, values = [0..4]
 
-    if end == start: # เหลือค่าเดียว
-        return (0, values[start], values[end], start + 1, end)
+    if end == start: # เหลือวันเดียว
+        return (0, values[start], values[end], start, end)
+    if end - start == 1: #เหลือสองวัน
+        buy_day = None
+        sell_day = None
+        prof = None
+        if values[end] > values[start]:
+            buy_day = start
+            sell_day = end
+            prof = values[end] - values[start]
+        elif values[end] < values[start]:
+            buy_day = start
+            sell_day = start
+            prof = 0
+        else:
+            buy_day = start
+            sell_day = start
+            prof = 0
+        min_value = values[buy_day]
+        max_value = values[sell_day]
+        return (prof, min_value, max_value, buy_day, sell_day)
+        
 
     divider = (end + start)//2
     prof_l, min_l, max_l, buy_l, sell_l = keng_kumrai_Divide_And_Conquer(start, divider)
     prof_r, min_r, max_r, buy_r, sell_r = keng_kumrai_Divide_And_Conquer(divider + 1, end)
     return keng_kumrai_Combine(min_l, max_l, prof_l, min_r, max_r, prof_r, buy_l, sell_l, buy_r, sell_r)
 
+
     
 def keng_kumrai_Combine(min_l: int, max_l: int, prof_l: int, min_r: int, max_r: int, prof_r: int, buy_l: int, sell_l: int, buy_r: int, sell_r: int):
 
-    profit = max(prof_l, prof_r, max_r - min_l)
+    # profit = max(prof_l, prof_r, max_r - min_l)
+    min_value = None
+    max_value = None
 
+    '''
+    if A > B and A > C, then print A.
+    else if B > A and B > C, then print B.
+    else print C.
+    '''
+
+    profit = None
     buy_day = None
     sell_day = None
-    if profit == prof_l:
-        buy_day = buy_l
-        sell_day = sell_l
-    elif profit == prof_r:
+
+    prof_lr = max_r - min_l
+    # prof_l = max_l - min_l
+    # prof_r = max_r - min_r
+
+    if prof_r > prof_l and prof_r > prof_lr: # prof_r is max
         buy_day = buy_r
         sell_day = sell_r
-    else:
+        profit = prof_r
+    elif prof_l > prof_r and prof_l > prof_lr: # prof_l is max
+        buy_day = buy_l
+        sell_day = sell_l
+        profit = prof_l
+    elif prof_lr > prof_r and prof_lr > prof_l: # prof_lr is max
         buy_day = buy_l
         sell_day = sell_r
-    # if buy_day < sell_day:
-    return (profit, min(min_l, min_r), max(max_l, max_r), buy_day, sell_day)
+        profit = prof_lr
+    elif prof_r == prof_lr == prof_l: # เท่ากันสามอัน
+        # print("prof_r == prof_lr == prof_l")
+        buy_day = buy_l
+        sell_day = sell_r
+        profit = prof_lr
+    elif prof_r == prof_lr: # ขวาเท่ากับกลาง 
+        # print("prof_r == prof_lr")
+        buy_day = buy_l
+        sell_day = sell_r
+        profit = prof_lr
+    elif prof_l == prof_lr: # ซ้ายเท่ากับกลาง 
+        # print("prof_l == prof_lr")
+        buy_day = buy_l
+        sell_day = sell_r
+        profit = prof_lr
+    elif prof_r == prof_l: # ซ้ายเท่ากับขวา
+        # print("prof_l == prof_r")
+        buy_day = buy_l
+        sell_day = sell_l
+        profit = prof_l
+        
+        '''
+        suuuuuuuuuuuuuuuuu suuuuuuuuuuuuuuuuuuu mwl
+        '''
+    min_value = values[buy_day]
+    max_value = values[sell_day]
+    # profit = max(prof_l, prof_r, max_r - min_l)
+    return (profit, min_value, max_value, buy_day, sell_day)
 
     
 print("----------------------------------------------------------------------------------------")
 print("Brute Force method:")
-keng_kumrai_Brute_Force()
+# keng_kumrai_Brute_Force()
 print("----------------------------------------------------------------------------------------")
 print("Divide and Conquer method:")
 max_profit, min_value, max_value, buy_day, sell_day = keng_kumrai_Divide_And_Conquer(0, days - 1)
 max_profit = "{:.2f}".format(max_profit)
+
 print("Best day to buy : " + str(buy_day + 1))
-print("Best value to buy : " + str(values[buy_day]))
+print("Best value to buy : " + str(min_value))
 print("Best day to sell : " + str(sell_day + 1))
-print("Best value to sell : " + str(values[sell_day]))
+print("Best value to sell : " + str(max_value))
 print("Best profit : " + str(max_profit))
 print("Number of days until sell : " + str(sell_day - buy_day))
 # i hate this problem so muchhhhhhhh
